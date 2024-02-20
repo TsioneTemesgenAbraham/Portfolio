7b0847e0f922a9da38386ca390f5be397aef1f6e
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2024-02-20 18:42:46 +0000

    Added navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,107 @@
+import { React, useState, useEffect } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Instagram from "../assets/instagram-white-icon.svg";
+import Facebook from "../assets/facebook-app-round-white-icon.svg";
+import Linkedin from "../assets/linkedin-app-white-icon.svg";
+import { BrowserRouter as Router } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
+function NavBar() {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <video autoPlay loop muted>
+            <source src="/videos/logo.mp4" type="video/mp4" />
+          </video>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#about"
+                className={
+                  activeLink === "about" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("about")}
+              >
+                About
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="/some/valid/uri">
+                  <img src={Facebook} alt="" />
+                </a>
+                <a href="/some/valid/uri">
+                  <img src={Instagram} alt="" />
+                </a>
+                <a href="/some/valid/uri">
+                  <img src={Linkedin} alt="" />
+                </a>
+              </div>
+              <HashLink to="#connect">
+                <button className="vvd">
+                  <span>Let’s Connect</span>
+                </button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  );
+}
+
+export default NavBar;
