94b0341d3a27696bcd542c4133e41f6ff0afd875
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2024-02-22 23:17:56 +0000

    Added about page

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+function About() {
+  return (
+    <section className="about">
+      <div>
+        <h2>About</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry. Lorem Ipsum has been
+          the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
