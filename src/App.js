39af2d81faafd5bcab3f1a15e5ccea3c8f240196
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2024-02-22 23:18:58 +0000

    Added styling for the pages added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Poster from "./components/Poster";
+import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -9,7 +13,11 @@ function App() {
     <div className="App">
       <NavBar />
       <Poster />
+      <About />
       <Skills />
+      <Projects />
+      <Form />
+      <Footer />
     </div>
   );
 }
