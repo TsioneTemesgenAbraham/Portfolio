390d73c07d4c9ee74348465c530a856b36e7519f
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2024-02-26 21:30:39 +0000

    Added link

--- src/components/PCards.js
+++ src/components/PCards.js
@@ -1,12 +1,14 @@
 import { Col } from "react-bootstrap";
 
-export const PCards = ({ title, description, imgUrl, alt }) => {
+export const PCards = ({ title, description, imgUrl, href, alt }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} alt={alt} />
         <div className="proj-txtx">
-          <h4>{title}</h4>
+          <h4>
+            <a href={href}>{title}</a>
+          </h4>
           <span>{description}</span>
         </div>
       </div>
